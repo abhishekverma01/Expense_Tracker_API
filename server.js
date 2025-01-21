83cb45f9c2368da8baa2d111bc4f3b0bb068098f
abhishekverma01 <156703912+abhishekverma01@users.noreply.github.com>
2025-01-22 00:52:44 +0530

    port fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 
 // Middlewares
 app.use(cors());
