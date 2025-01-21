b94ff35c799c768da04d59882e7031809be47f79
abhishekverma01 <156703912+abhishekverma01@users.noreply.github.com>
2025-01-22 00:55:17 +0530

    port fix

--- server.js
+++ server.js
@@ -34,12 +34,6 @@ app.use((req, res, next) => {
     res.status(404).json({ error: "Route not found" });
 });
 
-// Global error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Something went wrong!" });
-});
-
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}.`);
 });
